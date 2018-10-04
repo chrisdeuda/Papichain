a3e5879e9bb3e63a990bed23ec9cb41b48ab35f7
Christopher Deuda <christopherdeuda@gmail.com>
2018-10-04 23:39:50 +0800

    Add methods in Blockchain model

--- app/Models/Blockchain.js
+++ app/Models/Blockchain.js
@@ -1,45 +1,70 @@
 'use strict'
 
+const Block = use('App/Models/Block')
 const Model = use('Model')
 
-class Blockchain extends Model {
 
-    
+class Blockchain extends Model{
+    static _bootIfNotBooted () {
+        if (this.name !== 'MyModel') {
+          super._bootIfNotBooted()
+        }
+      }
 
     constructor(){
-
+        super()
         // create chain transactions
 
-        this.chain = []
+        // Create function for the genesis Block
+        // Mining Genesis Block
+        var newBlock =  Blockchain.genesisBlock();
+        this.chain = [newBlock];
         this.current_transactions = []
 
         // Binding of this
 
-        this.newBlock = this.newBlock.bind(this);
-        this.newTransactions = this.newTransactions.bind(this)
-        this.lastBlock = this.lastBlock.bind(this)
-        this.proofOfWork = this.proofOfWork.bind(this)
-
-
-    }
-
-    newBlock(){
+        this.newBlock = this.newBlock("123", "123")
+        this.newTransactions = this.newTransactions()
+        this.lastBlock = this.lastBlock()
+        //this.proofOfWork = this.proofOfWork.bind(this)
+        console.log("Initilized Block Success")
 
     }
 
-    newTransactions(){
+    static genesisBlock(){
 
+        var NewBlock = new Block(0, "01/01/2018", "Genesis block", "0");
+        console.log(NewBlock);
+        return NewBlock;
+    }
+    
+    newBlock(proof, previousHash){
+        const block = {
+            index: this.chain.length + 1,
+            timestamp: new Date().toString(),
+            transactions: this.current_transactions,
+            proof: proof,
+            previous_hash:  previousHash
+        }
+        this.current_transactions = [],
+        this.chain.push(block)
     }
 
-    hash(){
+      newTransactions(sender, recipient, amount){
+
+        this.current_transactions.push({
+            sender: sender,
+            recipient: recipient,
+            amount: amount
+        })
+        console.log("New transactions", this.current_transactions)
+        return this.lastBlock('index') + 1
 
     }
-
+    
     lastBlock(){ // return the last block }
-
-
-
-
+        return this.chain.slice(-1)[0]
+    }
 
 }
 
