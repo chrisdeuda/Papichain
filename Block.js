304a2ec7862ae86d3b880497654dde4cb8e7a186
Christopher Deuda <christopherdeuda@gmail.com>
2018-10-05 21:48:27 +0800

    Add mining difficulty mining

--- Block.js
+++ Block.js
@@ -10,14 +10,31 @@ class Block {
         this.data = data
         this.previousHash = previousHash
         this.hash = this.calculateHash();
+        // Random value
         this.nonce = 0
     }
 
      calculateHash(){
         var hash = SHA256( this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)+ this.nonce).toString();
-        console.log(hash);
         return hash;
     }    
+
+    /**
+     * Proof of Work Implementation 
+     * 
+     * To avoid the spam DDOS Attack
+     * 
+     * @param {*} difficulty 
+     */
+    mineBlock(difficulty){
+        // Calculate until there is a zero in the hash
+        while( this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")){
+            this.nonce++;
+            this.hash  = this.calculateHash();
+        }
+        console.log("Number of try" + this.nonce)
+        console.log("Block mined:" + this.hash);
+    }
 
     
 }
