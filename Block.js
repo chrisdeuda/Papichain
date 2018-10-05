75037258e28ea9aee3d9d222932a7bf32f151ef2
Christopher Deuda <christopherdeuda@gmail.com>
2018-10-05 21:07:52 +0800

    Remove the adonis framework

---
+++ Block.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const SHA256 = require("crypto-js/sha256")
+
+class Block {
+
+    constructor( index, timestamp, data, previousHash){
+        this.index = index
+        this.timestamp = timestamp
+        this.data = data
+        this.previousHash = previousHash
+        this.hash = Block.calculateHash();
+        this.nonce = 0
+    }
+
+    static calculateHash(){
+        var hash = SHA256( this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)+ this.nonce).toString();
+        console.log(hash);
+        return hash;
+    }    
+
+    
+}
+
+module.exports = Block
